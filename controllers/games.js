10538f9a61e77313531d4252ca2b926788d31161
gabogara <gabo1gara@gmail.com>
2025-12-12 21:28:03 -0800

    chore: scaffold games and reviews controllers

---
+++ controllers/games.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const Game = require("../models/game.js");
+
+module.exports = router;
---
+++ controllers/reviews.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const Review = require("../models/review.js");
+
+module.exports = router;

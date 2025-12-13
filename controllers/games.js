07b82a7def508abab8ac584c62d5b653e85bf35e
gabogara <gabo1gara@gmail.com>
2025-12-12 23:41:37 -0800

    fix: move games views directory to correct location

--- controllers/games.js
+++ controllers/games.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const Game = require("../models/game.js");
 
+// INDEX PAGE
+// GET /games
+router.get("/", async (req, res) => {
+  res.render("games/index.ejs");
+});
+
 module.exports = router;

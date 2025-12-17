99b3eb1d5bc407c05d3a10b4813100f3c0c875ac
gabogara <gabo1gara@gmail.com>
2025-12-17 12:54:40 -0800

    feat: add routes to edit and update current user profile

--- controllers/users.js
+++ controllers/users.js
@@ -26,4 +26,32 @@ router.get("/:userId", async (req, res) => {
   }
 });
 
+// EDIT PROFILE PAGE
+// GET /users/me/edit
+router.get("/me/edit", async (req, res) => {
+  try {
+    res.locals.profileUser = await User.findById(req.session.user._id);
+    res.render("users/edit.ejs");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+// UPDATE PROFILE
+// PUT /users/me
+router.put("/me", async (req, res) => {
+  try {
+    await User.updateOne(
+      { _id: req.session.user._id },
+      { bio: req.body.bio, favoriteGenre: req.body.favoriteGenre }
+    );
+
+    res.redirect(`/users/${req.session.user._id}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
 module.exports = router;

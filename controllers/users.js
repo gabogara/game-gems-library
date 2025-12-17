421fb6101d012489447d6394569f48c5bd8e20d6
gabogara <gabo1gara@gmail.com>
2025-12-17 11:03:22 -0800

    feat: add users controller for profile page

---
+++ controllers/users.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+const Game = require("../models/game.js");
+const Review = require("../models/review.js");
+
+// USER PROFILE
+// GET /users/:userId
+router.get("/:userId", async (req, res) => {
+  try {
+    res.locals.profileUser = await User.findById(req.params.userId);
+
+    res.locals.games = await Game.find({ owner: req.params.userId }).sort({
+      createdAt: -1,
+    });
+
+    res.locals.reviews = await Review.find({ author: req.params.userId })
+      .populate("game", "title")
+      .sort({ createdAt: -1 });
+
+    res.render("users/show.ejs");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
+
+module.exports = router;
